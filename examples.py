d278a8dd3b1bd9060065eff49a3b7999acd396da
Ilya Potapov <elias.potapov@gmail.com>
2017-12-06 19:35:13 +0200

    Examples: remove extra dependencies, calculate just HFD

--- examples.py
+++ examples.py
@@ -2,30 +2,33 @@
 
 """
 Some basic examples of HFD usage.
+Note: extra dependency: Matplotlib
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 from hfd import hfd, curve_length, lin_fit_hfd
-import tretsa
 
 def apply_hfd(x,name=None):
+    print("##################")
+    print("#### "+str(name))
+    print("##################")
     ## HFD optimized
     print("*** HFD optimized")
     t = time.time()
-    print("\tHFD=",-hfd(x,opt=True))
+    print("\tHFD=",hfd(x,opt=True))
     print("\ttiming:",time.time() - t)
     ## HFD, not optimized
     print("*** HFD non-optimized")
     t = time.time()
-    print("\tHFD=",-hfd(x,opt=False))
+    print("\tHFD=",hfd(x,opt=False))
     print("\ttiming:",time.time() - t)
     ##
     print("*** step-by-step (not-optimized)")
     t = time.time()
     k, L = curve_length(x,opt=False)
-    HFD = -lin_fit_hfd(np.log2(k),np.log2(L),log=False)
+    HFD = lin_fit_hfd(np.log2(k),np.log2(L),log=False)
     print("\tHFD=",HFD)
     print("\ttiming:",time.time()-t)
     plt.plot(np.log2(k),np.log2(L),'.')
@@ -37,22 +40,6 @@ def apply_hfd(x,name=None):
 
     return HFD;
 
-def apply_dfa(x,name=None):
-    print("*** DFA")
-    t = time.time()
-    f,l = tretsa.DFA(x,return_logarithms=True)
-    print("\tDFA time:",time.time() - t)
-    alpha = np.polyfit(l,f,deg=1)[0]
-    print("\talpha(DFA) =",alpha)
-    plt.plot(l,f,'.')
-    plt.annotate("alpha = "+str(alpha),(0.05,0.9),xycoords='axes fraction')
-    plt.xlabel('Log length')
-    plt.ylabel('Log fluctuation')
-    if name:
-        plt.title(name)
-
-    return alpha;
-
 ### Higuchi original data: random Gaussian increments = Brownian
 N = 2**16
 z = np.random.randn(N+1000)
@@ -60,36 +47,27 @@ x = np.empty(N,dtype=np.float)
 for i in range(N):
     x[i] = np.sum(z[:(i+1000)])
 
-plt.subplot(421)
-apply_dfa(x,"Brownian")
-plt.subplot(422)
+plt.subplot(221)
 apply_hfd(x,"Brownian")
 
 ### Gaussian
 N = 2**11
 x = np.random.randn(N)
 
-plt.subplot(423)
-apply_dfa(x,"Gaussian")
-plt.subplot(424)
+plt.subplot(222)
 apply_hfd(x,"Gaussian")
 
 ### Pink noise
 N = 2**17
-x = tretsa.util.generate_data_with_scaling(1.0,N=N)
+x = np.loadtxt('pink.txt')
 
-plt.subplot(425)
-apply_dfa(x,"Pink")
-plt.subplot(426)
+plt.subplot(223)
 apply_hfd(x,"Pink")
 
 ### Heart data
-x = np.loadtxt('test.txt')
+x = np.loadtxt('heart.txt')
 x = x[x<180]
-# plt.plot(x)
 
-plt.subplot(427)
-apply_dfa(x,"Heart")
-plt.subplot(428)
-apply_hfd(x,"Heart")
+plt.subplot(224)
+apply_hfd(x,"Heart: inter-beat intervals")
 plt.show()
